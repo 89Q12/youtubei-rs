845ad78716ba9eb48a581de680dba449440fcf04
11tuvork28 <zofi@xamh.de>
2022-05-23 00:25:50 +0200

    added video struct

---
+++ src/types/video.rs
@@ -0,0 +1,18 @@
+pub struct Video{
+   pub  title: String,
+   pub  id: String,
+   pub  author: String,
+   pub  ucid: String,
+   pub  published: String,
+   pub  views: String,
+   pub  description_html: String,
+   pub  length_seconds: String,
+   pub  live_now: String,
+   pub  premium: String,
+   pub  premiere_timestamp: String,
+   pub  author_verified: String,
+}
+
+impl Video {
+    
+}
