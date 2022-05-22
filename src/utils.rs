d1d14f651d3b8374f46519331e4c4fcf8cf778da
11tuvork28 <zofi@xamh.de>
2022-05-22 13:04:18 +0200

    added merge function

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,26 @@
+use serde_json::Value;
 use crate::types::clientConfig::ClientConfig;
 use crate::types::clientTypes;
 
 pub fn default_client_config() -> ClientConfig {
     ClientConfig::new(clientTypes::ClientTypes::Web,"US".to_string(),"US".to_string())
+}
+// Used to merge 2 values into one, probably could be optimized
+pub fn merge(a: &mut Value, b: &Value) {
+    match (a, b) {
+        (Value::Object(ref mut a), &Value::Object(ref b)) => {
+            for (k, v) in b {
+                merge(a.entry(k).or_insert(Value::Null), v);
+            }
+        }
+        (Value::Array(ref mut a), &Value::Array(ref b)) => {
+            a.extend(b.clone());
+        }
+        (Value::Array(ref mut a), &Value::Object(ref b)) => {
+            a.extend([Value::Object(b.clone())]);
+        }
+        (a, b) => {
+            *a = b.clone();
+        }
+    }
 }
