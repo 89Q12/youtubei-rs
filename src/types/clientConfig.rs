cbd6b8418e08e26b2a76fe9eb1c0a438256052f1
11tuvork28 <zofi@xamh.de>
2022-05-22 17:49:19 +0200

    removed unsued derive and added client in new fn

--- src/types/clientConfig.rs
+++ src/types/clientConfig.rs
@@ -1,14 +1,30 @@
 use super::clientTypes::ClientTypes;
-use serde::{Deserialize, Serialize};
-#[derive(Serialize, Deserialize)]
+use reqwest::Client;
+use reqwest::header;
 pub struct ClientConfig{
    pub client_type: ClientTypes,
    pub region: String,
    pub proxy_region:String,
+   pub http_client: Client
 }
 
 impl ClientConfig {
-    pub fn new(client_type: ClientTypes, region: String, proxy_region: String) -> Self { Self { client_type, region, proxy_region } }
+    // Constructs a new ClientConfig with the client and all required headers
+    pub fn new(client_type: ClientTypes, region: String, proxy_region: String) -> Self {
+        let mut headers = header::HeaderMap::new();
+        headers.insert("accept-charset", header::HeaderValue::from_static("ISO-8859-1,utf-8;q=0.7,*;q=0.7"));
+        headers.insert("accept", header::HeaderValue::from_static("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"));
+        headers.insert("accept-language", header::HeaderValue::from_static("en-us,en;q=0.5"));
+        headers.insert("x-youtube-client-name", header::HeaderValue::from_static("1"));
+        headers.insert("x-youtube-client-version", header::HeaderValue::from_static( "2.20200609"));
+        headers.insert("cookie",header::HeaderValue::from_static("CONSENT=YES+"));
+        let _http_client = reqwest::ClientBuilder::new()
+        .https_only(true)
+        .user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36")
+        .default_headers(headers)
+        .build().unwrap();
+        Self { client_type, region, proxy_region, http_client: _http_client} 
+    }
 
     pub fn name(&self) ->  String {
        self.client_type.get_client_type().name
