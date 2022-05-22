ba7e8a8c988f562efeb91ec943fcf9f0b7e4d68d
11tuvork28 <zofi@xamh.de>
2022-05-22 18:55:02 +0200

    added gzip and reorderd headers

--- src/types/clientConfig.rs
+++ src/types/clientConfig.rs
@@ -12,8 +12,10 @@ impl ClientConfig {
     // Constructs a new ClientConfig with the client and all required headers
     pub fn new(client_type: ClientTypes, region: String, proxy_region: String) -> Self {
         let mut headers = header::HeaderMap::new();
-        headers.insert("accept-charset", header::HeaderValue::from_static("ISO-8859-1,utf-8;q=0.7,*;q=0.7"));
+        headers.insert("Content-Type", header::HeaderValue::from_static("application/json; charset=UTF-8"));
+        headers.insert("Accept-Encoding", header::HeaderValue::from_static("gzip"));
         headers.insert("accept", header::HeaderValue::from_static("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"));
+        headers.insert("accept-charset", header::HeaderValue::from_static("ISO-8859-1,utf-8;q=0.7,*;q=0.7"));
         headers.insert("accept-language", header::HeaderValue::from_static("en-us,en;q=0.5"));
         headers.insert("x-youtube-client-name", header::HeaderValue::from_static("1"));
         headers.insert("x-youtube-client-version", header::HeaderValue::from_static( "2.20200609"));
@@ -22,6 +24,7 @@ impl ClientConfig {
         .https_only(true)
         .user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36")
         .default_headers(headers)
+        .gzip(true)
         .build().unwrap();
         Self { client_type, region, proxy_region, http_client: _http_client} 
     }
