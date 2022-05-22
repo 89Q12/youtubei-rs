e7b25872343d94221f09bb9e2a60ef2dc605c0df
11tuvork28 <zofi@xamh.de>
2022-05-23 00:26:20 +0200

    add search_channel struct

---
+++ src/types/search_channel.rs
@@ -0,0 +1,14 @@
+pub struct search_channel{
+    author: String,
+    ucid: String,
+    author_thumbnail: String,
+    subscriber_count: String,
+    video_count: String,
+    description_html: String,
+    auto_generated: String,
+    author_verified: String,
+}
+
+impl search_channel {
+    
+}
