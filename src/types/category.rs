d3b4eededd285abe569668ad36dab2e8672a9490
11tuvork28 <zofi@xamh.de>
2022-05-23 10:47:03 +0200

    Added correct type definition to category

--- src/types/category.rs
+++ src/types/category.rs
@@ -1,6 +1,14 @@
+use super::{video::{Video}, search_channel::{SearchChannel}, search_playlist::SearchPlaylist};
+
+pub enum CategoryTypes{
+    Video(Video),
+    SearchChannel(SearchChannel),
+    SearchPlaylists(SearchPlaylist)
+
+}
 pub struct Category{
    pub  title: String,
-   pub  contents: String,
+   pub  contents: Vec<CategoryTypes>,
    pub  description_html: String,
    pub  url: String,
    pub  badges: String,
