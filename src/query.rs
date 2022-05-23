55d9ba11c708b7aca8119b057531981de5430df6
11tuvork28 <zofi@xamh.de>
2022-05-23 13:30:25 +0200

    Added Result as return type

--- src/query.rs
+++ src/query.rs
@@ -1,15 +1,17 @@
+use std::fmt::Error;
+
 use crate::types::query_results::{SearchResult, CommentsQuery, VideoQuery, ChannelQuery};
 pub fn search(query: String) -> SearchResult{
  todo!()
 }
 
-pub fn get_comments(continuation:String) ->CommentsQuery{
+pub fn get_comments(continuation:String) ->Result<CommentsQuery,  Error>{
     todo!()
 }
 
-pub fn get_video(video_id:String, params: String) ->VideoQuery{
+pub fn get_video(video_id:String, params: String) ->Result<VideoQuery,  Error>{
  todo!()
 }
-pub fn get_channel(browse_id:String, url: String) ->ChannelQuery{
+pub fn get_channel(browse_id:String, url: String) ->Result<ChannelQuery,  Error>{
 todo!()
 }
