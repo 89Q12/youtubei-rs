67a9d7cdb2de6e862b0f34d4b6b5782a0705e988
11tuvork28 <zofi@xamh.de>
2022-05-23 13:17:48 +0200

    boostratped query.rs

---
+++ src/query.rs
@@ -0,0 +1,15 @@
+use crate::types::query_results::{SearchResult, CommentsQuery, VideoQuery, ChannelQuery};
+pub fn search(query: String) -> SearchResult{
+ todo!()
+}
+
+pub fn get_comments(continuation:String) ->CommentsQuery{
+    todo!()
+}
+
+pub fn get_video(video_id:String, params: String) ->VideoQuery{
+ todo!()
+}
+pub fn get_channel(browse_id:String, url: String) ->ChannelQuery{
+todo!()
+}
