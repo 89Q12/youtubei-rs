8d61bbb2f711d7a09ab5d4ac5b59435a6db0fcef
11tuvork28 <zofi@xamh.de>
2022-05-23 13:16:37 +0200

    added query results probably not in final form

---
+++ src/types/query_results.rs
@@ -0,0 +1,26 @@
+use super::{search_video::SearchVideo, search_playlist::SearchPlaylist, search_channel::SearchChannel, video::Video, channel::Channel};
+
+pub enum SearchResult {
+    VideoRenderer(SearchVideo),
+    PlaylistRenderer(SearchPlaylist),
+    SearchChannel(SearchChannel)
+}
+
+
+pub struct SearchQuery{
+    pub continuation: String,
+    pub results: Vec<SearchResult>,
+}
+pub struct CommentsQuery{
+
+}
+pub struct VideoQuery{
+    pub continuation: String,
+    pub videos:Video,
+    pub related_videos: Vec<SearchVideo>,
+}
+pub struct ChannelQuery{
+    pub continuation: String,
+    pub channel: Channel,
+    
+}
