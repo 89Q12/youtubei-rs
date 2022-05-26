03a2271dc525ebba282ab85207276ec7873ddb8b
11tuvork28 <zofi@xamh.de>
2022-05-26 17:29:39 +0200

    refactored struct

--- src/types/search_video.rs
+++ src/types/search_video.rs
@@ -1,16 +1,15 @@
-use super::endpoints::{EndpointWatch, EndpointBrowse, EndpointNext};
+use super::endpoints::{EndpointWatch, EndpointBrowse};
 
 pub struct SearchVideo{
     pub title: String,
     pub id: String,
     pub channel_name: String,
-    pub description: String,
     pub author: String,
     pub author_verified: bool,
     pub channel_thumbnail: String,
     pub view_count_text: String,
     pub length_text: String,
+    pub thumbnail: String,
     pub endpoint: EndpointWatch,
     pub browse_channel: EndpointBrowse,
-    pub comments_continuation: EndpointNext
 }
