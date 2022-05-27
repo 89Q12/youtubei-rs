abc0cc6b5b4f9d9987e8fabaebd7f2d021a2d64e
11tuvork28 <zofi@xamh.de>
2022-05-27 13:27:18 +0200

    made fields public

--- src/types/channel_video.rs
+++ src/types/channel_video.rs
@@ -1,14 +1,14 @@
 use super::endpoints::EndpointWatch;
 
 pub struct ChannelVideo{
-    title: String, 
-    id: String, 
-    published_text: String, 
-    author: String, 
-    author_verified: bool,
-    thumbnail:String,
-    view_count_text:  String,
-    length_text: String,
-    channel_thumbnail: String, 
-    endpoint: EndpointWatch
+    pub title: String, 
+    pub id: String, 
+    pub published_text: String, 
+    pub author: String, 
+    pub author_verified: bool,
+    pub thumbnail:String,
+    pub view_count_text:  String,
+    pub length_text: String,
+    pub channel_thumbnail: String, 
+    pub endpoint: EndpointWatch
 }
