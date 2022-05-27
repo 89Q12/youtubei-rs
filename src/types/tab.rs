d7cf776d9656adb9d676fcef558d8c4584a9b16b
11tuvork28 <zofi@xamh.de>
2022-05-27 13:04:50 +0200

    added continuation

--- src/types/tab.rs
+++ src/types/tab.rs
@@ -3,5 +3,6 @@ use super::category::CategoryTypes;
 pub struct ChannelTab{
     pub title: String,
     pub selected: bool,
-    pub content: Vec<CategoryTypes>
+    pub content: Vec<CategoryTypes>,
+    pub continuation: String,
 }
