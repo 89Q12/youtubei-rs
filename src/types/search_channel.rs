f26fddcd9f2d05ec6e8beb41d24d0e44b2107a5b
11tuvork28 <zofi@xamh.de>
2022-05-28 12:56:14 +0200

    updated types

--- src/types/search_channel.rs
+++ src/types/search_channel.rs
@@ -5,7 +5,7 @@ pub struct SearchChannel{
    pub ucid: String,
    pub author_thumbnail: String,
    pub subscriber_count: String,
-   pub video_count: i32,
+   pub video_count:String,
    pub description_html: String,
    pub auto_generated: bool,
    pub author_verified: bool,
