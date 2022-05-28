f0d1b87c9f5090b5402c892e5d0e74b2fccdb78c
11tuvork28 <zofi@xamh.de>
2022-05-28 20:31:00 +0200

    refactored videos structs into one file

--- src/types/video.rs
+++ src/types/video.rs
@@ -1,5 +1,4 @@
-use super::video_player::VideoPlayer;
-
+use super::endpoints::{EndpointWatch, EndpointBrowse};
 pub struct Video{
    pub  title: String,
    pub  id: String,
@@ -17,6 +16,48 @@ pub struct Video{
    pub thumbnail: String,
 }
 
-impl Video {
-    
+
+pub struct VideoPlayer{
+   pub formats: Vec<Format>,
+   pub apdaptiveformts: Vec<Format>,
+}
+
+pub struct Format{
+   pub itag: i64,
+   pub url: String,
+   pub mime_type: String,
+   pub bitrate: i64,
+   pub quality: String,
+   pub fps: i64,
+   pub quality_label: String,
+   pub audio_quality: String,
+}
+
+
+pub struct SearchVideo{
+    pub title: String,
+    pub id: String,
+    pub channel_name: String,
+    pub published_text: String,
+    pub author: String,
+    pub author_verified: bool,
+    pub channel_thumbnail: String,
+    pub view_count_text: String,
+    pub length_text: String,
+    pub thumbnail: String,
+    pub endpoint: EndpointWatch,
+    pub browse_channel: EndpointBrowse,
+}
+
+pub struct ChannelVideo{
+    pub title: String, 
+    pub id: String, 
+    pub published_text: String, 
+    pub author: String, 
+    pub author_verified: bool,
+    pub thumbnail:String,
+    pub view_count_text:  String,
+    pub length_text: String,
+    pub channel_thumbnail: String, 
+    pub endpoint: EndpointWatch
 }
