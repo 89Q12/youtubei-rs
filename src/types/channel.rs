158bc6ae867587f6280012f6062794ef08d1faad
11tuvork28 <zofi@xamh.de>
2022-05-28 20:32:45 +0200

    refactored channel related structs into one file

--- src/types/channel.rs
+++ src/types/channel.rs
@@ -1,4 +1,4 @@
-use super::tab::ChannelTab;
+use super::{video::ChannelVideo, playlist::SearchPlaylist, endpoints::EndpointBrowse};
 
 pub struct Channel{
     pub name: String,
@@ -6,4 +6,38 @@ pub struct Channel{
     pub banner: String,
     pub avatar: String,
     pub description: String,
+}
+pub struct CommunityPost {
+    pub content_text: String,
+    pub content_attachment: String,
+    pub author_name: String,
+    pub author_thumbnail: String,
+    pub vote_count: i64,
+    pub published_time_text: String,
+    pub browse_endpoint: EndpointBrowse,
+}
+pub struct SearchChannel{
+    pub author: String,
+    pub ucid: String,
+    pub author_thumbnail: String,
+    pub subscriber_count: String,
+    pub video_count:String,
+    pub description_html: String,
+    pub auto_generated: bool,
+    pub author_verified: bool,
+    pub endpoint: EndpointBrowse,
+ }
+ 
+pub struct ChannelTab{
+    pub title: String,
+    pub selected: bool,
+    pub content: Vec<TabTypes>,
+    pub continuation: String,
+}
+pub enum TabTypes{
+    Video(ChannelVideo),
+    SearchChannel(SearchChannel),
+    SearchPlaylist(SearchPlaylist),
+    PostCommunity(CommunityPost)
+
 }
--- src/types/tab.rs
+++
@@ -1,8 +0,0 @@
-use super::category::CategoryTypes;
-
-pub struct ChannelTab{
-    pub title: String,
-    pub selected: bool,
-    pub content: Vec<CategoryTypes>,
-    pub continuation: String,
-}
