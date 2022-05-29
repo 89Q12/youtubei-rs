cb228c105be96d8f5ccea3096182b80aa287e796
11tuvork28 <zofi@xamh.de>
2022-05-29 13:09:28 +0200

    removed author verified

--- src/types/playlist.rs
+++ src/types/playlist.rs
@@ -7,7 +7,6 @@ pub struct SearchPlaylist{
     pub ucid:String,
     pub video_count: String,
     pub thumbnail: String,
-    pub author_verified: bool,
     pub play_endpoint: EndpointWatch,
     pub browse_endpoint: EndpointBrowse,
 }
