46e7d6022de57c383559602f4548165bbbdaeb55
11tuvork28 <zofi@xamh.de>
2022-05-31 11:27:27 +0200

    added documentation

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,7 +1,7 @@
 use serde_json::{json, Value};
 use crate::types::client::ClientConfig;
 use crate::utils::merge;
-
+/// Prepears the data and makes a post request to the browse endpoint
 pub(crate) async fn browse_continuation(continuation : &str, client_config : &ClientConfig) -> Value{
   
   let data = json!({
@@ -10,6 +10,7 @@ pub(crate) async fn browse_continuation(continuation : &str, client_config : &Cl
   });
   return post_json("/youtubei/v1/browse", data, client_config).await;
 }
+/// Prepears the data and makes a post request to the browse endpoint
 pub(crate)  async fn browse_browseid(browse_id : &str, params:  &str, client_config : &ClientConfig) -> Value{
   
   let data = json!({
@@ -19,6 +20,7 @@ pub(crate)  async fn browse_browseid(browse_id : &str, params:  &str, client_con
   });
   return post_json("/youtubei/v1/browse", data, client_config).await;
 }
+/// Prepears the data and makes a post request to the next endpoint
 pub(crate)  async fn next(continuation : &str, client_config: &ClientConfig) -> Value{
   let data = json!({
     "context"      : make_context(&client_config),
@@ -26,10 +28,12 @@ pub(crate)  async fn next(continuation : &str, client_config: &ClientConfig) -> 
   });
   return post_json("/youtubei/v1/next", data, &client_config).await;
 }
+/// Prepears the data and makes a post request to the next endpoint
 pub(crate)  async fn next_with_data(mut data: serde_json::Value, client_config: &ClientConfig) -> Value{
   merge(&mut data, &json!({"context": make_context(client_config)}));
   return post_json("/youtubei/v1/next", data, &client_config).await;
 }
+/// Prepears the data and makes a post request to the player endpoint
 pub(crate)  async fn player(video_id: &str, params:  &str,client_config: &ClientConfig) -> Value{
   let data = json!({
     "videoId" : video_id,
@@ -38,7 +42,8 @@ pub(crate)  async fn player(video_id: &str, params:  &str,client_config: &Client
   });
   return post_json("/youtubei/v1/player", data, &client_config).await;
 }
-
+/// Prepears the data and makes a post request to the resolve_url endpoint,
+/// which returns another endpoint to query
 pub(crate)  async fn resolve_url(url: &str, client_config: &ClientConfig) -> Value{
   let data = json!({
     "context" : make_context(&client_config),
@@ -46,7 +51,7 @@ pub(crate)  async fn resolve_url(url: &str, client_config: &ClientConfig) -> Val
   });
   return post_json("/youtubei/v1/navigation/resolve_url", data, &client_config).await;
 }
-
+/// Prepears the data and makes a post request to the search endpoint
 pub(crate)  async fn search(search_query: &str, params:  &str, client_config: &ClientConfig) -> Value{
   let data = json!({
     "query"   : search_query,
@@ -55,6 +60,7 @@ pub(crate)  async fn search(search_query: &str, params:  &str, client_config: &C
   });
   return post_json("/youtubei/v1/search", data, client_config).await;
 }
+/// Prepears the data and makes a post request to the search endpoint
 pub(crate)  async fn search_continuation(continuation : &str, client_config: &ClientConfig) -> Value{
   let data = json!({
     "context"      : make_context(&client_config),
@@ -62,6 +68,7 @@ pub(crate)  async fn search_continuation(continuation : &str, client_config: &Cl
   });
   return post_json("/youtubei/v1/search", data, &client_config).await;
 }
+/// takes the client_config and makes a json object that is used as context for the api call.
 fn make_context(client_config: &ClientConfig) -> serde_json::Value{
   let mut client_context = json!({
       "hl"           : "en",
@@ -77,7 +84,11 @@ if client_config.client_type.get_client_type().screen == "EMBED"{
 }
 return json!({"client": &client_context});
 }
-
+/// Makes a post request to the given endpoint with the given data and parameters
+/// On failure, returns 
+/// ```json
+/// {"error": message}
+/// ```
 async fn post_json(endpoint: &str, data: Value, client_config : &ClientConfig) -> Value {
   let url = endpoint.to_owned()+"?key="+ &client_config.client_type.get_client_type().api_key.to_owned() +"&prettyPrint=false";
   let wrapped_response=  client_config.http_client
