1b242e59ce1bbfabad7f58015909389c9fcd907e
11tuvork28 <zofi@xamh.de>
2022-05-31 11:29:52 +0200

    added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
+/// contains functions to generate the data to make an API call.
+/// private because they return raw data and or errors
 pub(crate) mod endpoints;
+/// Contains all the structs used to represent the data returned by the API
 pub mod types;
+/// Contains utilities for various tasks
 pub mod utils;
+/// private because they are intended to be called by query functions only
 pub(crate) mod extractors;
+/// Contains the main functions of this library
+/// Refer to the tests for usage information
 pub mod query;
+/// Contains test functions
 mod tests;
