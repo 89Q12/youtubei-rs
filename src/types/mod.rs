0a031f10ef45db52f472adfeeefda25189389801
11tuvork28 <zofi@xamh.de>
2022-05-31 10:55:18 +0200

    added documentation

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,6 +1,12 @@
+/// Client related structs and enums
 pub mod client;
+/// Video related structs and enums
 pub mod video;
+/// Playlist related structs and enums
 pub mod playlist;
+/// Endpoint related structs and enums
 pub mod endpoints;
+/// Query result related structs and enums
 pub mod query_results;
+/// Channel related structs and enums
 pub mod channel;
