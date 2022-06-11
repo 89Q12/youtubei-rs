d8b3b7ea422f07ce6532f79b32278290825f5b15
11tuvork28 <zofi@xamh.de>
2022-06-11 15:59:51 +0200

    Added CompactRadioRenderer aka mixes

--- src/types/playlist.rs
+++ src/types/playlist.rs
@@ -57,3 +57,14 @@ pub struct GridPlaylistRenderer{
     pub owner_badges:Option<Vec<BadgeRendererVec>>,
     pub navigation_endpoint: NavigationEndpoint,
 }
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CompactRadioRenderer{
+    pub navigation_endpoint: NavigationEndpoint,
+    pub playlist_id: String,
+    pub secondary_navigation_endpoint: NavigationEndpoint,
+    pub thumbnail: Thumbnails,
+    pub title: SimpleText,
+    pub video_count_short_text: Runs,
+    pub video_count_text: Runs,
+}
