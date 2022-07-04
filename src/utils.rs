9db409215e12aac289da708bc930a3b874e0845b
11tuvork28 <zofi@xamh.de>
2022-07-04 15:31:39 +0200

    Added utils functions fixes #5

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use serde_json::Value;
 use crate::types::client::ClientConfig;
-use crate::types::client;
+use crate::types::{client, self};
+use crate::types::query_results::NextResult;
 
 pub fn default_client_config() -> ClientConfig {
     ClientConfig::new(client::ClientTypes::Web,"US".to_string(),"US".to_string())
@@ -47,5 +48,206 @@ pub fn unwrap_to_i64(input:  Option<i64>) -> i64{
     match input{
         Some(n) => n,
         None => 0,
+    }
+}
+
+/// Utility function to get likes for a video
+pub fn get_likes(video: &NextResult) -> String {
+    match video
+        .contents
+        .as_ref()
+        .unwrap()
+        .two_column_watch_next_results
+        .as_ref()
+        .unwrap()
+        .results
+        .results
+        .contents
+        .get(0)
+        .unwrap()
+    {
+        types::misc::NextContents::VideoPrimaryInfoRenderer(vpir) => match vpir
+            .video_actions
+            .menu_renderer
+            .as_ref()
+            .unwrap()
+            .top_level_buttons
+            .as_ref()
+            .unwrap()
+            .get(0)
+            .unwrap()
+        {
+            types::misc::TopLevelButtons::ButtonRenderer(_) => unreachable!(),
+            types::misc::TopLevelButtons::ToggleButtonRenderer(btn) => btn
+                .default_text
+                .accessibility
+                .as_ref()
+                .unwrap()
+                .accessibility_data
+                .label
+                .clone(),
+        },
+        _ => unreachable!(),
+    }
+}
+/// Utility function to build the description for a video
+pub fn get_description(video: &NextResult) -> String {
+    let mut desc = String::from("");
+    match video
+        .contents
+        .as_ref()
+        .unwrap()
+        .two_column_watch_next_results
+        .as_ref()
+        .unwrap()
+        .results
+        .results
+        .contents
+        .get(1)
+        .unwrap()
+    {
+        types::misc::NextContents::VideoSecondaryInfoRenderer(vsir) => {
+            for text in vsir.description.runs.iter() {
+                desc.push_str(text.text.clone().as_str());
+            }
+        }
+        _ => unreachable!(),
+    }
+    desc
+}
+/// Utility function to check if the author is verified
+pub fn get_author_verified(video: &NextResult) -> bool {
+    match video
+        .contents
+        .as_ref()
+        .unwrap()
+        .two_column_watch_next_results
+        .as_ref()
+        .unwrap()
+        .results
+        .results
+        .contents
+        .get(1)
+        .unwrap()
+    {
+        types::misc::NextContents::VideoSecondaryInfoRenderer(vsir) => {
+            match &vsir.owner.video_owner_renderer.badges {
+                Some(badges) => match badges
+                    .get(0)
+                    .unwrap()
+                    .metadata_badge_renderer
+                    .icon
+                    .as_ref()
+                    .unwrap()
+                    .icon_type
+                    .as_str()
+                {
+                    "OFFICIAL_ARTIST_BADGE" => true,
+                    "CHECK_CIRCLE_THICK" => true,
+                    _ => false,
+                },
+                None => false,
+            }
+        }
+        _ => unreachable!(),
+    }
+}
+/// Utility function to get the subscriber count
+pub fn get_subcribe_count(video: &NextResult) -> String {
+    match video
+        .contents
+        .as_ref()
+        .unwrap()
+        .two_column_watch_next_results
+        .as_ref()
+        .unwrap()
+        .results
+        .results
+        .contents
+        .get(1)
+        .unwrap()
+    {
+        types::misc::NextContents::VideoSecondaryInfoRenderer(vsir) => {
+            match &vsir.owner.video_owner_renderer.subscriber_count_text {
+                Some(text) => text.simple_text.clone(),
+                None => String::from(""),
+            }
+        }
+        _ => unreachable!(),
+    }
+}
+/// Utility function to get the thumbnail of the video owner
+pub fn get_owner_thumbnail(video: &NextResult) -> String {
+    match video
+        .contents
+        .as_ref()
+        .unwrap()
+        .two_column_watch_next_results
+        .as_ref()
+        .unwrap()
+        .results
+        .results
+        .contents
+        .get(1)
+        .unwrap()
+    {
+        types::misc::NextContents::VideoSecondaryInfoRenderer(vsir) => vsir
+            .owner
+            .video_owner_renderer
+            .thumbnail
+            .thumbnails
+            .get(0)
+            .unwrap()
+            .url
+            .clone(),
+        _ => unreachable!(),
+    }
+}
+/// Utility function to  the continuation of the video comments
+pub fn get_continuation_comments(video: &NextResult) -> Option<String> {
+    match video
+        .contents
+        .as_ref()
+        .unwrap()
+        .two_column_watch_next_results
+        .as_ref()
+        .unwrap()
+        .results
+        .results
+        .contents
+        .last()
+        .unwrap()
+    {
+        types::misc::NextContents::ContinuationItemRenderer(cir) => {
+            match &cir.continuation_endpoint.continuation_command {
+                Some(cmd) => Some(cmd.token.clone()),
+                None => None,
+            }
+        }
+        _ => None,
+    }
+}
+/// Utility function to  the continuation of related videos 
+pub fn get_continuation_related(video: &NextResult) -> Option<String> {
+    match video
+        .contents
+        .as_ref()
+        .unwrap()
+        .two_column_watch_next_results
+        .as_ref()
+        .unwrap()
+        .results
+        .results
+        .contents
+        .last()
+        .unwrap()
+    {
+        types::misc::NextContents::ContinuationItemRenderer(cir) => {
+            match &cir.continuation_endpoint.continuation_command {
+                Some(cmd) => Some(cmd.token.clone()),
+                None => None,
+            }
+        }
+        _ => None,
     }
 }
